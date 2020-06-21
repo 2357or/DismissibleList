efb677247baadca49b55999a590e82ef2c232233
2357or <ryoooo2357g@gmail.com>
2020-06-21 17:11:07 +0900

    Initial Commit

---
+++ DismissibleList/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DismissibleList
+//
+//  Created by 大槻亮太 on 2020/06/21.
+//  Copyright © 2020 大槻亮太. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
